df2b0bccb1b498208f3343497e849c2b00ab05db
Richard Cats <phreak.spyder@gmail.com>
2017-12-07 15:58:10 +0100

    Added SQLite test database and connection

---
+++ DevicesEnStoringen/DataAccess/DataCommander.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SQLite;
+
+namespace DevicesEnStoringen.DataAccess
+{
+
+    class DataCommander
+    {
+        public event Action ContainersReady; //Delegate maar dan zonder parameters
+
+
+        private readonly SQLiteConnection conn;
+        private string connString = ConfigurationManager.ConnectionStrings["DevicesEnStoringen"].ToString();
+
+        public DataCommander()
+        {
+            conn = new SQLiteConnection(connString);
+        }
+
+        public List<string> GetAllContainers()
+        {
+            List<string> list = new List<string>();
+
+            string sql = "SELECT * FROM devices";
+
+            SQLiteCommand command = new SQLiteCommand(conn);
+            command.CommandText = sql;
+
+            conn.Open();
+
+            SQLiteDataReader datareader;
+
+            using (datareader = command.ExecuteReader())
+            {
+                while (datareader.Read())
+                {
+                    string result = String.Format("Device: {0}, {1}, {2}, {3}, {4}",
+                        datareader.GetInt32(0),
+                        datareader.GetString(1),
+                        datareader.GetString(2),
+                        datareader.GetString(3),
+                        datareader.GetString(4));
+
+                    list.Add(result);
+                }
+            }
+            conn.Close();
+
+
+            if (ContainersReady != null)
+            {
+                ContainersReady.Invoke();
+            }
+            return list;
+        }
+    }
+}
---
+++ DevicesEnStoringen/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevicesEnStoringen.DataAccess;
+
+namespace DevicesEnStoringen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        DataCommander dc = new DataCommander();
+        List<string> containers = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dc.ContainersReady += showContainers;
+        }
+
+        private void showContainers()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string item in containers)
+            {
+                sb.AppendLine(item);
+            }
+            txtContainer.Text = sb.ToString();
+        }
+
+        private void btnGetData_Click(object sender, RoutedEventArgs e)
+        {
+            containers = dc.GetAllContainers();
+        }
+    }
+}
