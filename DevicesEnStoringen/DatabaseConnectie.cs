5c053f249cbd932949f1d2378755905abf5cddb8
Richard Cats <phreak.spyder@gmail.com>
2017-12-19 14:03:36 +0100

    Added a login screen

---
+++ DevicesEnStoringen/DatabaseConnectie.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+
+namespace DevicesEnStoringen
+{
+    class DatabaseConnectie
+    {
+        private SQLiteConnection conn;
+        private string connString = ConfigurationManager.ConnectionStrings["DevicesEnStoringen"].ToString();
+
+        public void OpenConection()
+        {
+            conn = new SQLiteConnection(connString);
+            conn.Open();
+        }
+        public void CloseConnection()
+        {
+            conn.Close();
+        }
+        public void ExecuteQueries(string Query_)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(Query_, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        public SQLiteDataReader DataReader(string Query_)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(Query_, conn);
+            SQLiteDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+        public object ShowDataInGridView(string Query_)
+        {
+            SQLiteDataAdapter dr = new SQLiteDataAdapter(Query_, connString);
+            DataSet ds = new DataSet();
+            dr.Fill(ds);
+            object dataum = ds.Tables[0];
+            return dataum;
+        }
+    }
+}
---
+++ DevicesEnStoringen/Inloggen.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DevicesEnStoringen
+{
+    /// <summary>
+    /// Interaction logic for Inloggen.xaml
+    /// </summary>
+    public partial class Inloggen : Window
+    {
+        public Inloggen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInloggen_Click(object sender, RoutedEventArgs e)
+        {
+            DatabaseConnectie conn = new DatabaseConnectie();
+            //conn.OpenConection();
+
+            SQLiteConnection con = new SQLiteConnection(@"Data Source=C:\Users\Richard\Documents\Visual Studio 2017\Projects\devices-en-storingen\DevicesEnStoringen\Data\DevicesEnStoringen.sqlite;Version=3");
+            try
+            {
+                if (con.State == System.Data.ConnectionState.Closed)
+                    con.Open();
+                string query = "SELECT COUNT(1) FROM Medewerker WHERE Emailadres=@Emailadres AND Wachtwoord=@Wachtwoord";
+                SQLiteCommand sqlCmd = new SQLiteCommand(query, con);
+                sqlCmd.CommandType = System.Data.CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Emailadres", txtGebruikersnaam.Text);
+                sqlCmd.Parameters.AddWithValue("@Wachtwoord", txtWachtwoord.Password);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+
+                if (count == 1)
+                {
+                    MainWindow mainwindow = new MainWindow();
+                    mainwindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Gebruikersnaam of wachtwoord is incorrect");
+                }
+            }
+            catch(Exception)
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
