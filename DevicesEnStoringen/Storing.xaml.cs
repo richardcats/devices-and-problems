8c9bdcb2533c928a1d0dd81e76980b7613a53e5f
Richard Cats <phreak.spyder@gmail.com>
2017-12-29 16:14:28 +0100

    The user can now filter the datagrid

---
+++ DevicesEnStoringen/Storing.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DevicesEnStoringen
+{
+    /// <summary>
+    /// Interaction logic for Storing.xaml
+    /// </summary>
+    public partial class Storing : Window
+    {
+        bool editMode;
+        public Storing(bool editMode)
+        {
+            InitializeComponent();
+            this.editMode = editMode;
+
+            if (editMode)
+                Title = "Storing bewerken";
+            else
+                Title = "Storing registreren";
+        }
+    }
+}
