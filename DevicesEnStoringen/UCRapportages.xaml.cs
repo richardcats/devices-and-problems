184384588fa91bd682bbcab911bb917a1859d4d5
Richard Cats <phreak.spyder@gmail.com>
2018-01-13 15:29:30 +0100

    Filled the comboxes to make a report

--- DevicesEnStoringen/UCRapportages.xaml.cs
+++ DevicesEnStoringen/UCRapportages.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,46 @@ namespace DevicesEnStoringen
     /// </summary>
     public partial class UCRapportages : UserControl
     {
+        public static ObservableCollection<string> listStoringYear;
+        public static ObservableCollection<string> listStoringMonth;
+
         public UCRapportages()
         {
             InitializeComponent();
+
+            cboStoringJaar.ItemsSource = FillComboboxStoringYear();
+            cboStoringMaand.ItemsSource = FillComboboxStoringMonth();
+        }
+
+        public ObservableCollection<string> FillComboboxStoringYear()
+        {
+            listStoringYear = new ObservableCollection<string>();
+            DatabaseConnectie conn = new DatabaseConnectie();
+            conn.OpenConnection();
+            SQLiteDataReader dr = conn.DataReader("SELECT strftime('%Y', DatumToegevoegd) as Year FROM Storing GROUP BY Year");
+
+            while (dr.Read())
+                listStoringYear.Add(dr["Year"].ToString());
+
+            return listStoringYear;
+        }
+
+        public ObservableCollection<string> FillComboboxStoringMonth()
+        {
+            listStoringMonth = new ObservableCollection<string>();
+            DatabaseConnectie conn = new DatabaseConnectie();
+            conn.OpenConnection();
+            SQLiteDataReader dr = conn.DataReader("SELECT strftime('%m', DatumToegevoegd) as Month FROM Storing WHERE strftime('%Y', DatumToegevoegd) ='" + cboStoringJaar.SelectedValue + "' GROUP BY Month");
+
+            while (dr.Read())
+                listStoringMonth.Add(dr["Month"].ToString());
+
+            return listStoringMonth;
+        }
+
+        private void cboStoringJaar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cboStoringMaand.IsEnabled = true;
         }
     }
 }
